7343f13e07411c755f0d162f873f2ced532189b1
Macnelson9 <michaelofatu@gmail.com>
2025-03-17 18:24:30 +0100

    Add FAQ collapse/expand functionality and enhance styling for various sections

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// FAQ'S collapse/expand function
+document.querySelectorAll('.faq-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const faqCard = header.parentElement;
+        faqCard.classList.add('active');
+        
+        // Close other FAQ items
+        document.querySelectorAll('.faq-card').forEach(card => {
+            if (card !== faqCard) {
+                card.classList.remove('active');
+            }
+        });
+    });
+});
