1187b44859129632695c6768215a874b2399590c
Macnelson9 <michaelofatu@gmail.com>
2025-03-20 14:16:34 +0100

    Refactor mobile navigation and FAQ functionality; enhance styles for improved user experience

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 'use strict';
 
+// Selected Elements
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const closeBtn = document.getElementById('times-btn');
+const mobileNav = document.querySelector('.mobile-navigation');
+
+// Handle mobile navigation
+document.addEventListener('DOMContentLoaded', function () {
+    if (hamburgerBtn) {
+        hamburgerBtn.addEventListener('click', () => {
+            mobileNav.classList.add('show');
+        });  
+    }
+
+    if (closeBtn) {
+        closeBtn.addEventListener('click', () => {
+            mobileNav.classList.remove('show');
+        });
+    }
+})
+
 // FAQ'S collapse/expand function
 document.querySelectorAll('.faq-header').forEach(header => {
     header.addEventListener('click', () => {
         const faqCard = header.parentElement;
-        faqCard.classList.add('active');
-        
-        // Close other FAQ items
-        document.querySelectorAll('.faq-card').forEach(card => {
-            if (card !== faqCard) {
+
+        if (faqCard.classList.contains('active')) {
+            faqCard.classList.remove('active');
+        } else {
+            document.querySelectorAll('.faq-card').forEach(card => {
                 card.classList.remove('active');
-            }
-        });
+            });
+            
+            faqCard.classList.add('active');
+        }
     });
 });
